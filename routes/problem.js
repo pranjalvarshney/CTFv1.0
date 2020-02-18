2ed1f21df00c1e93e96e96a569708614df3b79bd
Pranjal Varshney <pranjal25varshney@gmail.com>
2020-02-19 00:38:23 +0530

    problem 4 added

--- routes/problem.js
+++ routes/problem.js
@@ -3,11 +3,20 @@ const router = express.Router();
 const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
 
 
-router.get('/pikachu0', ensureAuthenticated, (req, res) => res.render('pikachu0'));
+router.get('/pikachu0', ensureAuthenticated, (req, res) =>{ 
+    res.render('pikachu0')
+});
+
 router.get('/pikachu1', ensureAuthenticated, (req, res) => res.render('pikachu1'));
+
 router.get('/pikachu2', ensureAuthenticated, (req, res) => res.render('pikachu2'));
+
 router.get('/pikachu3', ensureAuthenticated, (req, res) => res.render('pikachu3'));
+
 router.get('/pikachu4', ensureAuthenticated, (req, res) => res.render('pikachu4'));
+
 router.get('/pikachu5', ensureAuthenticated, (req, res) => res.render('pikachu5'));
+
 router.get('/pikachu6', ensureAuthenticated, (req, res) => res.render('pikachu6'));
+
 module.exports = router;
