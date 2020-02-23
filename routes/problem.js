bbe9bce61717e5e2e8639d611ad7a20e432abb6f
Pranjal Varshney <pranjal25varshney@gmail.com>
2020-02-23 15:58:49 +0530

    other access denied and granted added

--- routes/problem.js
+++ routes/problem.js
@@ -6,213 +6,240 @@ const User=require('../models/User');
 
 router.get('/pikachu0', ensureAuthenticated, (req, res) =>res.render('pikachu0',{user: req.user}));
 router.post('/pikachu0',ensureAuthenticated,(req,res)=>{
-	if(req.user.level0== false && req.user.time > 0 ){
-		if(req.body.user=='abc' && req.body.password=='123'){
-			if( req.user.time >=  9000000-300000)
-			{
-				req.user.points += 100;
-				req.user.level0=true;
-				req.user.save();
-				res.redirect('/success');
-			}
-			else if( req.user.time< 90000000-300000 && req.user.time>= 9000000-1800000){
-				req.user.points += (100-((9000000-req.user.time)/30000));
-				req.user.level0=true;
-				req.user.save()
-				res.redirect('/success')
-			}
-			else{
-				req.user.points += 30;
-				req.user.level0=true;
-				req.user.save()
-				res.redirect('/success')	
+	if( req.user.time > 0){
+		if(req.user.level0== false ){
+			if(req.body.user=='abc' && req.body.password=='123'){
+				if( req.user.time >=  9000000-300000)
+				{
+					req.user.points += 100;
+					req.user.level0=true;
+					req.user.save();
+					res.redirect('/success');
+				}
+				else if( req.user.time< 90000000-300000 && req.user.time>= 9000000-1800000){
+					req.user.points += (100-((9000000-req.user.time)/30000));
+					req.user.level0=true;
+					req.user.save()
+					res.redirect('/success')
+				}
+				else{
+					req.user.points += 30;
+					req.user.level0=true;
+					req.user.save()
+					res.redirect('/success')	
+				}
+			}else{
+				res.redirect('/retry');
 			}
 		}else{
-			res.redirect('/retry');
+			res.redirect('/solved');
 		}
 	}else{
-		res.redirect('/solved');
+
 	}
 })
 router.get('/pikachu1', ensureAuthenticated, (req, res) => res.render('pikachu1',{user: req.user}));
 router.post('/pikachu1',ensureAuthenticated,(req,res)=>{
-	if(req.user.level1 == false && req.user.time > 0){
-		if(req.body.password=='answer'){
-			if( req.user.time>=  9000000-600000)
-			{
-				req.user.points += 100;
-				req.user.level1=true;
-				req.user.save()
-				res.redirect('/success')
-			}
-			else if( req.user.time< 90000000-600000 && req.user.time>= 9000000-1200000){
-				req.user.points += (100-((9000000-req.user.time)/30000));
-				req.user.level1=true;
-				req.user.save()
-				res.redirect('/success')
-			}
-			else{
-				req.user.points += 30;
-				req.user.level1=true;
-				req.user.save()
-				res.redirect('/success')	
+	if( req.user.time > 0){
+		if(req.user.level1 == false){
+			if(req.body.password=='answer'){
+				if( req.user.time>=  9000000-600000)
+				{
+					req.user.points += 100;
+					req.user.level1=true;
+					req.user.save()
+					res.redirect('/success')
+				}
+				else if( req.user.time< 90000000-600000 && req.user.time>= 9000000-1200000){
+					req.user.points += (100-((9000000-req.user.time)/30000));
+					req.user.level1=true;
+					req.user.save()
+					res.redirect('/success')
+				}
+				else{
+					req.user.points += 30;
+					req.user.level1=true;
+					req.user.save()
+					res.redirect('/success')	
+				}
+			}else{
+				res.redirect('/retry');
 			}
 		}else{
-			res.redirect('/retry');
+			res.redirect('/solved');
 		}
 	}else{
-		res.redirect('/solved');
 
 	}
 })
 router.get('/pikachu2', ensureAuthenticated, (req, res) => res.render('pikachu2',{user: req.user}));
 router.post('/pikachu2',ensureAuthenticated,(req,res)=>{
-	if(req.user.level2 == false && req.user.time > 0){
-		if(req.body.password == 'answer'){
-			if( req.user.time>=  9000000-1200000)
-			{
-				req.user.points += 100;
-				req.user.level3=true;
-				req.user.save()
-				res.redirect('/success')
-			}
-			else if( req.user.time< 90000000-1200000 && req.user.time>= 9000000-2100000){
-				req.user.points += (100-((9000000-req.user.time)/30000));
-				req.user.level3=true;
-				req.user.save()
-				res.redirect('/success')
-			}
-			else{
-				req.user.points += 30;
-				req.user.level3=true;
-				req.user.save()
-				res.redirect('/success')	
+	if( req.user.time > 0){
+		if(req.user.level2 == false){
+			if(req.body.password == 'answer'){
+				if( req.user.time>=  9000000-1200000)
+				{
+					req.user.points += 100;
+					req.user.level3=true;
+					req.user.save()
+					res.redirect('/success')
+				}
+				else if( req.user.time< 90000000-1200000 && req.user.time>= 9000000-2100000){
+					req.user.points += (100-((9000000-req.user.time)/30000));
+					req.user.level3=true;
+					req.user.save()
+					res.redirect('/success')
+				}
+				else{
+					req.user.points += 30;
+					req.user.level3=true;
+					req.user.save()
+					res.redirect('/success')	
+				}
+			}else{
+				res.redirect('/retry');
 			}
 		}else{
-			res.redirect('/retry');
+			res.redirect('/solved');
 		}
 	}else{
-		res.redirect('/solved');
+
 	}
 })
 router.get('/pikachu3', ensureAuthenticated, (req, res) => res.render('pikachu3',{user: req.user}));
 router.post('/pikachu3',ensureAuthenticated,(req,res)=>{
-	if(req.user.level3 == false && req.user.time > 0){
-		if(req.body.password == 'answer'){
-			if( req.user.time>=  9000000-2100000)
-			{
-				req.user.points += 100;
-				req.user.level3=true;
-				req.user.save()
-				res.redirect('/success')
-			}
-			else if( req.user.time< 90000000-300000 && req.user.time>= 9000000-900000){
-				req.user.points += (100-((9000000-req.user.time)/30000));
-				req.user.level3=true;
-				req.user.save()
-				res.redirect('/success')
-			}
-			else{
-				req.user.points += 30;
-				req.user.level3=true;
-				req.user.save()
-				res.redirect('/success')	
+	if( req.user.time > 0){
+		if(req.user.level3 == false){
+			if(req.body.password == 'answer'){
+				if( req.user.time>=  9000000-2100000)
+				{
+					req.user.points += 100;
+					req.user.level3=true;
+					req.user.save()
+					res.redirect('/success')
+				}
+				else if( req.user.time< 90000000-300000 && req.user.time>= 9000000-900000){
+					req.user.points += (100-((9000000-req.user.time)/30000));
+					req.user.level3=true;
+					req.user.save()
+					res.redirect('/success')
+				}
+				else{
+					req.user.points += 30;
+					req.user.level3=true;
+					req.user.save()
+					res.redirect('/success')	
+				}
+			}else{
+				res.redirect('/retry');
 			}
 		}else{
-			res.redirect('/retry');
+			res.redirect('/solved');
 		}
 	}else{
-		res.redirect('/solved');
+
 	}
 })
 router.get('/pikachu4', ensureAuthenticated, (req, res) => res.render('pikachu4',{user: req.user}));
 router.post('/pikachu4',ensureAuthenticated,(req,res)=>{
-	if(req.user.level4 == false && req.user.time > 0){
-		if(req.body.password == 'answer'){
-			if( req.user.time>=  9000000-300000)
-			{
-				req.user.points += 100;
-				req.user.level4=true;
-				req.user.save()
-				res.redirect('/success')
-			}
-			else if( req.user.time< 90000000-300000 && req.user.time>= 9000000-900000){
-				req.user.points += (100-((9000000-req.user.time)/30000));
-				req.user.level4=true;
-				req.user.save()
-				res.redirect('/success')
-			}
-			else{
-				req.user.points += 30;
-				req.user.level4=true;
-				req.user.save()
-				res.redirect('/success')	
+	if( req.user.time > 0){
+		if(req.user.level4 == false){
+			if(req.body.password == 'answer'){
+				if( req.user.time>=  9000000-300000)
+				{
+					req.user.points += 100;
+					req.user.level4=true;
+					req.user.save()
+					res.redirect('/success')
+				}
+				else if( req.user.time< 90000000-300000 && req.user.time>= 9000000-900000){
+					req.user.points += (100-((9000000-req.user.time)/30000));
+					req.user.level4=true;
+					req.user.save()
+					res.redirect('/success')
+				}
+				else{
+					req.user.points += 30;
+					req.user.level4=true;
+					req.user.save()
+					res.redirect('/success')	
+				}
+			}else{
+				res.redirect('/retry');
 			}
 		}else{
-			res.redirect('/retry');
+			res.redirect('/solved');
 		}
 	}else{
-		res.redirect('/solved');
+
 	}
 })
 router.get('/pikachu5', ensureAuthenticated, (req, res) => res.render('pikachu5',{user: req.user}));
 router.post('/pikachu5',ensureAuthenticated,(req,res)=>{
-	if(req.user.level5 == false && req.user.time > 0){
-		if(req.body.password == 'answer'){
-			if( req.user.time>=  9000000-300000)
-			{
-				req.user.points += 100;
-				req.user.level5=true;
-				req.user.save()
-				res.redirect('/success')
-			}
-			else if( req.user.time< 90000000-300000 && req.user.time>= 9000000-900000){
-				req.user.points += (100-((9000000-req.user.time)/30000));
-				req.user.level5=true;
-				req.user.save()
-				res.redirect('/success')
-			}
-			else{
-				req.user.points += 30;
-				req.user.level5=true;
-				req.user.save()
-				res.redirect('/success')	
+	if( req.user.time > 0){
+		if(req.user.level5 == false){
+			if(req.body.password == 'answer'){
+				if( req.user.time>=  9000000-300000)
+				{
+					req.user.points += 100;
+					req.user.level5=true;
+					req.user.save()
+					res.redirect('/success')
+				}
+				else if( req.user.time< 90000000-300000 && req.user.time>= 9000000-900000){
+					req.user.points += (100-((9000000-req.user.time)/30000));
+					req.user.level5=true;
+					req.user.save()
+					res.redirect('/success')
+				}
+				else{
+					req.user.points += 30;
+					req.user.level5=true;
+					req.user.save()
+					res.redirect('/success')	
+				}
+			}else{
+				res.redirect('/retry');
 			}
 		}else{
-			res.redirect('/retry');
+			res.redirect('/solved');
 		}
 	}else{
-		res.redirect('/solved');
+
 	}
 })
 router.get('/pikachu6', ensureAuthenticated, (req, res) => res.render('pikachu6',{user: req.user}));
 router.post('/pikachu6',ensureAuthenticated,(req,res)=>{
-	if(req.user.level6 == false && req.user.time > 0){
-		if(req.body.password == 'answer'){
-			if( req.user.time>=  9000000-300000)
-			{
-				req.user.points += 100;
-				req.user.level6=true;
-				req.user.save()
-				res.redirect('/success')
-			}
-			else if( req.user.time< 90000000-300000 && req.user.time>= 9000000-900000){
-				req.user.points += (100-((9000000-req.user.time)/30000));
-				req.user.level6=true;
-				req.user.save()
-				res.redirect('/success')
-			}
-			else{
-				req.user.points += 30;
-				req.user.level6=true;
-				req.user.save()
-				res.redirect('/success')	
+	if( req.user.time > 0){
+		if(req.user.level6 == false){
+			if(req.body.password == 'answer'){
+				if( req.user.time>=  9000000-300000)
+				{
+					req.user.points += 100;
+					req.user.level6=true;
+					req.user.save()
+					res.redirect('/success')
+				}
+				else if( req.user.time< 90000000-300000 && req.user.time>= 9000000-900000){
+					req.user.points += (100-((9000000-req.user.time)/30000));
+					req.user.level6=true;
+					req.user.save()
+					res.redirect('/success')
+				}
+				else{
+					req.user.points += 30;
+					req.user.level6=true;
+					req.user.save()
+					res.redirect('/success')	
+				}
+			}else{
+				res.redirect('/retry');
 			}
 		}else{
-			res.redirect('/retry');
+			res.redirect('/solved');
 		}
 	}else{
-		res.redirect('/solved');
+
 	}
 })
 module.exports = router;
